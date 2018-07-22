99b4b4d27f85e06f209811dfb7c928bec339c1e2
Torning, Fraser <Fraser_Torning@comcast.com>
2018-07-21 23:51:46 -0400

    Added logging and fs functions

--- liri.js
+++ liri.js
@@ -11,25 +11,36 @@ var searchParams = process.argv.slice(3);
 var twitterClient = new Twitter(keys.twitter);
 var spotifyClient = new Spotify(keys.spotify);
 
-
-switch (searchType.toLowerCase()) {
-    case 'my-tweets':
-        twitterRequest(searchParams);
-        break;
-    case 'spotify-this-song':
-        spotifyRequest(searchParams);
-        break;
-    case 'movie-this':
-        movieRequest(searchParams);
-        break;
-    case 'do-what-it-says':
-        console.log("do-what-it-says");
-        break;
-    default:
-        console.log("Unknown command: " + searchType)
-        console.log("\nthe following commands are supported:\nmy-tweets\nspotify-this-song <search string>\nmovie-this <search string>\ndo-what-it-says")
-        break;
+function main() {
+    switch (searchType.toLowerCase()) {
+        case 'my-tweets':
+            twitterRequest(searchParams);
+            break;
+        case 'spotify-this-song':
+            spotifyRequest(searchParams);
+            break;
+        case 'movie-this':
+            movieRequest(searchParams);
+            break;
+        case 'do-what-it-says':
+            fs.readFile("random.txt", "utf8", function(err, data) {
+                if (!err) {
+                    var dataSplit = data.split(",");
+                    searchType = dataSplit[0];
+                    searchParams = dataSplit[1].split(" ");
+                    main();
+                } else {
+                    console.log(err);
+                }
+            })
+            break;
+        default:
+            console.log("Unknown command: " + searchType)
+            console.log("\nthe following commands are supported:\nmy-tweets\nspotify-this-song <search string>\nmovie-this <search string>\ndo-what-it-says")
+            break;
+    }
 }
+
 
 function twitterRequest(searchParams=null) {
     if (!searchParams || searchParams.length < 1) {
@@ -40,9 +51,13 @@ function twitterRequest(searchParams=null) {
 
     twitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
+            logData(searchType + " " + searchParams);
             for (var i = tweets.length - 1; i >= 0; i--) {
                 console.log(tweets[i].created_at + "\t" + tweets[i].text);
+                logData(tweets[i]);
             }
+            
+            
         } else {
             console.log(error);
         }
@@ -67,6 +82,8 @@ function spotifyRequest(searchParams=null) {
             console.log("\nSong Title: " + res.name);
             console.log("\nPreview: " + res.preview_url);
             console.log("\nAlbum: " + res.album.name);
+            logData(searchType + " " + searchParams);
+                    logData(res);
         } else {
             console.log('Error occurred: ' + error);
         }
@@ -103,8 +120,22 @@ function movieRequest(searchParams=null) {
                     console.log("\nLanguage: " + body.Language);
                     console.log("\nPlot: " + body.Plot);
                     console.log("\nActors: " + body.Actors);
+                    logData(searchType + " " + searchParams);
+                    logData(body);
+                    
                 } else {
                     console.log("Error occured: " + error);
                 }
     })
 }
+
+function logData(data) {
+    fs.appendFile("log.txt", "\n" + data, function(err) {
+        if (err) {
+          console.log("Error encoutered: " + err);
+        }
+      });
+    
+}
+
+main();
